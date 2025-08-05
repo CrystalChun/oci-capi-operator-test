c38b5a1749db2d05be1a759c6b78c3eb77567808
CrystalChun <cchun@redhat.com>
2025-08-05 11:03:57 -0500

    Convert some variables to constants

--- internal/controllers/const.go
+++ internal/controllers/const.go
@@ -3,4 +3,7 @@ package controllers
 const (
 	OCICAPIClusterName  = "oci-capi-cluster"
 	capiSystemNamespace = "capi-system"
+
+	// Default Images
+	ClusterAutoscalerImage = "registry.k8s.io/autoscaling/cluster-autoscaler:v1.29.0"
 )
