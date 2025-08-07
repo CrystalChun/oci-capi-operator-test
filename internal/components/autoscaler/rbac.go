97cb03f6dfd0e90ed92df9da7af54ca577e6d8cb
CrystalChun <cchun@redhat.com>
2025-08-07 10:21:22 -0500

    Filled out more subcomponents and separated resources to use autoscaler into a different component

---
+++ internal/components/autoscaler/rbac.go
@@ -0,0 +1,74 @@
+package autoscaler
+
+import (
+	ocicapiv1alpha1 "github.com/openshift/oci-capi-operator/api/v1alpha1"
+
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func ServiceAccount(capiSystemNamespace string, scheme *runtime.Scheme, instance *ocicapiv1alpha1.OCIClusterAutoscaler) (client.Object, func() error) {
+	serviceAccount := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "oci-cluster-autoscaler",
+			Namespace: capiSystemNamespace,
+		},
+	}
+
+	mutateFn := func() error {
+		return controllerutil.SetControllerReference(instance, serviceAccount, scheme)
+	}
+
+	return serviceAccount, mutateFn
+}
+
+func ClusterRole(capiSystemNamespace string, instance *ocicapiv1alpha1.OCIClusterAutoscaler) (client.Object, func() error) {
+	clusterRole := &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "oci-cluster-autoscaler-extra",
+		},
+	}
+
+	mutateFn := func() error {
+		clusterRole.Rules = []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"infrastructure.cluster.x-k8s.io"},
+				Resources: []string{"*"},
+				Verbs:     []string{"get", "list", "watch", "update"},
+			},
+		}
+		return nil
+	}
+
+	return clusterRole, mutateFn
+}
+
+func ClusterRoleBinding(capiSystemNamespace string, instance *ocicapiv1alpha1.OCIClusterAutoscaler) (client.Object, func() error) {
+	clusterRoleBinding := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "oci-cluster-autoscaler-extra",
+		},
+	}
+
+	mutateFn := func() error {
+		clusterRoleBinding.RoleRef = rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "oci-cluster-autoscaler-extra",
+		}
+		clusterRoleBinding.Subjects = []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "oci-cluster-autoscaler",
+				Namespace: capiSystemNamespace,
+			},
+		}
+		return nil
+	}
+
+	return clusterRoleBinding, mutateFn
+}
