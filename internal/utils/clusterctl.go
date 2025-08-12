ecaa1253aa685d334898e17f7c0e253b515f21fe
CrystalChun <cchun@redhat.com>
2025-08-12 17:10:27 -0500

    Fix generate capi components

--- internal/utils/clusterctl.go
+++ internal/utils/clusterctl.go
@@ -11,39 +11,16 @@ import (
 
 // GenerateCAPIComponents uses the clusterctl library to generate all CAPI resources
 // for a given provider and returns the resources as a list of unstructured objects
-func GenerateCAPIComponents(ctx context.Context, provider string, providerType v1alpha3.ProviderType) ([]unstructured.Unstructured, error) {
+func GenerateCAPIComponents(ctx context.Context, provider string, providerType v1alpha3.ProviderType, namespace string) ([]unstructured.Unstructured, error) {
 	clusterctlClient, err := client.New(ctx, "")
 	if err != nil {
 		return nil, fmt.Errorf("error creating clusterctl client: %w", err)
 	}
-	components, err := clusterctlClient.GetProviderComponents(ctx, provider, providerType, client.ComponentsOptions{})
+	components, err := clusterctlClient.GetProviderComponents(ctx, provider, providerType, client.ComponentsOptions{
+		TargetNamespace: namespace,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("error getting provider components: %w", err)
 	}
 	return components.Objs(), nil
-	/*
-		 	// Create new clusterctl config client
-			configClient, err := configclient.New(ctx, "")
-			if err != nil {
-				return nil, fmt.Errorf("error creating clusterctl config client: %w", err)
-			}
-			// Create new clusterctl provider client
-			providerConfig, err := configClient.Providers().Get(provider, providerType)
-			if err != nil {
-				return nil, fmt.Errorf("error creating clusterctl provider client: %w", err)
-			}
-
-			// Initialize new clusterctl repository components
-			components, err := repository.NewComponents(repository.ComponentsInput{
-				Provider: providerConfig,
-				ConfigClient: configClient,
-				Options: repository.ComponentsOptions{
-					SkipTemplateProcess: true,
-				},
-			})
-			if err != nil {
-				return nil, fmt.Errorf("error creating clusterctl repository components: %w", err)
-			}
-			return components.Objs(), nil
-	*/
 }
