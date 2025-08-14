2b179f222c130abdde5cb1ed8d9a145cdf00f045
CrystalChun <cchun@redhat.com>
2025-08-14 14:17:31 -0500

    Deploy cluster autoscaler using helm

--- internal/components/autoscaler/autoscaler.go
+++ internal/components/autoscaler/autoscaler.go
@@ -2,45 +2,33 @@ package autoscaler
 
 import (
 	capiv1alpha1 "github.com/openshift/oci-capi-operator/api/v1alpha1"
-	ocicapiv1alpha1 "github.com/openshift/oci-capi-operator/api/v1alpha1"
 	"github.com/openshift/oci-capi-operator/internal/components"
 	"github.com/openshift/oci-capi-operator/internal/utils"
 
-	appsv1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/client-go/rest"
 )
 
-func NewComponent(namespace string, image string, autoscaler *capiv1alpha1.OCIClusterAutoscaler, scheme *runtime.Scheme) *components.Component {
-	deploy, deployMutateFn := AutoscalerDeployment(namespace, image, scheme, autoscaler)
-	serviceAccount, serviceAccountMutateFn := ServiceAccount(namespace, scheme, autoscaler)
-	clusterRole, clusterRoleMutateFn := ClusterRole(autoscaler)
-	clusterRoleBinding, clusterRoleBindingMutateFn := ClusterRoleBinding(namespace, autoscaler)
+// InstallAutoscaler installs the cluster-autoscaler Helm chart
+func InstallAutoscaler(values *AutoscalerDeploymentValues, restConfig *rest.Config) error {
+	valuesStr := GetValuesString(values)
+	err := utils.InstallHelmChart(values.Name, values.Namespace, values.RepositoryURL, values.Chart, valuesStr, restConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetComponents returns a Component for the cluster-autoscaler which includes a list of subcomponents
+func GetComponents(values *AutoscalerDeploymentValues, instance *capiv1alpha1.OCIClusterAutoscaler, scheme *runtime.Scheme) *components.Component {
+	clusterRole, clusterRoleMutateFn := ClusterRole(values.Name, instance)
+	clusterRoleBinding, clusterRoleBindingMutateFn := ClusterRoleBinding(values, instance)
 
 	return &components.Component{
 		Name: "Autoscaler",
 		Subcomponents: components.SubcomponentList{
-			{Name: "deployment", Object: deploy, MutateFn: deployMutateFn},
-			{Name: "serviceAccount", Object: serviceAccount, MutateFn: serviceAccountMutateFn},
 			{Name: "clusterRole", Object: clusterRole, MutateFn: clusterRoleMutateFn},
 			{Name: "clusterRoleBinding", Object: clusterRoleBinding, MutateFn: clusterRoleBindingMutateFn},
 		},
 	}
-}
-
-func AutoscalerDeployment(namespace string, image string, scheme *runtime.Scheme, instance *ocicapiv1alpha1.OCIClusterAutoscaler) (client.Object, func() error) {
-	deploy := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "oci-cluster-autoscaler",
-			Namespace: "oci-cluster-autoscaler",
-		},
-	}
-
-	mutateFn := func() error {
-		utils.SetDefaultLabels(deploy, instance.Name)
-		return nil
-	}
-
-	return deploy, mutateFn
 }
