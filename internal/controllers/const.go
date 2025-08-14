f644c4413e3fff4b654b7ea288adaa207a2c4402
CrystalChun <cchun@redhat.com>
2025-08-14 14:22:51 -0500

    Add deployment and service account names to constants

--- internal/controllers/const.go
+++ internal/controllers/const.go
@@ -1,15 +1,24 @@
 package controllers
 
 const (
-	FinalizerName = "ociclusterautoscaler.capi.openshift.io/finalizer"
+	FinalizerName  = "ociclusterautoscaler.capi.openshift.io/finalizer"
+	ManagedByLabel = "capi.openshift.io/managed-by"
 
 	OCICAPIClusterName    = "oci-capi-cluster"
 	CAPISystemNamespace   = "capi-system"
 	CAPOCISystemNamespace = "cluster-api-provider-oci-system"
 
+	CAPIDeploymentName   = "capi-manager"
+	CAPOCIDeploymentName = "capoci-controller-manager"
+
+	CAPIServiceAccountName   = "capi-manager"
+	CAPOCIServiceAccountName = "capoci-controller-manager"
+
 	CAPIWebhookServiceName   = "capi-webhook-service"
 	CAPOCIWebhookServiceName = "capoci-webhook-service"
 
 	// Default Images
-	ClusterAutoscalerImage = "registry.k8s.io/autoscaling/cluster-autoscaler:v1.29.0"
+	AutoscalerRepoURL        = "https://kubernetes.github.io/autoscaler"
+	AutoscalerChartName      = "oci-cluster-autoscaler/cluster-autoscaler"
+	AutoscalerDeploymentName = "oci-cluster-autoscaler"
 )
