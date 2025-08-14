b42e50e4d114eebc79bf70042bc94e10bb87ee63
CrystalChun <cchun@redhat.com>
2025-08-14 14:51:01 -0500

    Separate helm functions

--- internal/utils/helm.go
+++ internal/utils/helm.go
@@ -10,12 +10,7 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-func InstallHelmChart(name, namespace, url, chartName, values string, cfg *rest.Config) error {
-	chartRepo := repo.Entry{
-		Name: name,
-		URL:  url,
-	}
-
+func GetHelmClient(namespace string, cfg *rest.Config) (helmclient.Client, error) {
 	helmClient, err := helmclient.NewClientFromRestConf(&helmclient.RestConfClientOptions{
 		Options: &helmclient.Options{
 			Namespace: namespace,
@@ -23,20 +18,31 @@ func InstallHelmChart(name, namespace, url, chartName, values string, cfg *rest.
 		RestConfig: cfg,
 	})
 	if err != nil {
-		return fmt.Errorf("error creating helm client: %w", err)
+		return nil, fmt.Errorf("error creating helm client: %w", err)
 	}
+	return helmClient, nil
+}
 
-	err = helmClient.AddOrUpdateChartRepo(chartRepo)
+func AddChartRepo(helmClient helmclient.Client, name, url string) error {
+	chartRepo := repo.Entry{
+		Name: name,
+		URL:  url,
+	}
+	err := helmClient.AddOrUpdateChartRepo(chartRepo)
 	if err != nil {
 		return fmt.Errorf("error adding chart repo: %w", err)
 	}
+	return nil
+}
 
+func InstallHelmChart(helmClient helmclient.Client, name, namespace, chartName, values string) error {
 	release, err := helmClient.InstallChart(context.Background(), &helmclient.ChartSpec{
 		ReleaseName: name,
 		ChartName:   chartName,
 		Namespace:   namespace,
 		ValuesYaml:  values,
 		Wait:        true,
+		Version:     "9.45.0", // this "should" work with Kubernetes 1.32.0, ocp 4.19
 		Timeout:     300 * time.Second,
 	}, &helmclient.GenericHelmOptions{})
 	if err != nil {
