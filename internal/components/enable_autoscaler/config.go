df799ebcb07ae5fe799cd37f12fe637c5895cbd0
CrystalChun <cchun@redhat.com>
2025-08-25 20:40:57 -0500

    Fix enable autoscaler components

--- internal/components/enable_autoscaler/config.go
+++ internal/components/enable_autoscaler/config.go
@@ -1,5 +1,14 @@
 package enableautoscaler
 
+import (
+	"context"
+	"fmt"
+
+	ocicapioperatorv1alpha1 "github.com/openshift/oci-capi-operator/api/v1alpha1"
+	"github.com/openshift/oci-capi-operator/internal/utils"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
 type Config struct {
 	ClusterConfig     ClusterConfig
 	NetworkConfig     NetworkConfig
@@ -7,7 +16,7 @@ type Config struct {
 }
 
 type AutoScalingConfig struct {
-	CPUs     int32  `envconfig:"AUTOSCALER_CPUs" default:"2"`
+	CPUs     int32  `envconfig:"AUTOSCALER_CPUS" default:"2"`
 	Memory   int32  `envconfig:"AUTOSCALER_MEMORY" default:"4"`
 	MinNodes int32  `envconfig:"AUTOSCALER_MIN_NODES" default:"1"`
 	MaxNodes int32  `envconfig:"AUTOSCALER_MAX_NODES" default:"3"`
@@ -27,4 +36,53 @@ type NetworkConfig struct {
 	APIServerLoadBalancerID string `envconfig:"API_SERVER_LOAD_BALANCER_ID" default:""`
 	ClusterNetworkCIDRBlock string `envconfig:"CLUSTER_NETWORK_CIDR_BLOCK" default:""`
 	ServiceNetworkCIDRBlock string `envconfig:"SERVICE_NETWORK_CIDR_BLOCK" default:""`
+}
+
+func SetAutoScalingConfig(ctx context.Context, client client.Client, instance *ocicapioperatorv1alpha1.OCIClusterAutoscaler, config Config) (Config, error) {
+	if instance.Spec.Autoscaling.ShapeConfig != nil {
+		if instance.Spec.Autoscaling.ShapeConfig.CPUs != 0 {
+			config.AutoScalingConfig.CPUs = instance.Spec.Autoscaling.ShapeConfig.CPUs
+		}
+		if instance.Spec.Autoscaling.ShapeConfig.Memory != 0 {
+			config.AutoScalingConfig.Memory = instance.Spec.Autoscaling.ShapeConfig.Memory
+		}
+	}
+	if instance.Spec.Autoscaling.MinNodes != 0 {
+		config.AutoScalingConfig.MinNodes = instance.Spec.Autoscaling.MinNodes
+	}
+	if instance.Spec.Autoscaling.MaxNodes != 0 {
+		config.AutoScalingConfig.MaxNodes = instance.Spec.Autoscaling.MaxNodes
+	}
+
+	if instance.Spec.Autoscaling.Shape != "" {
+		config.AutoScalingConfig.Shape = instance.Spec.Autoscaling.Shape
+	}
+	if instance.Spec.Autoscaling.ImageID != "" {
+		config.AutoScalingConfig.ImageID = instance.Spec.Autoscaling.ImageID
+	}
+	if config, err := SetNetworkConfig(ctx, client, config); err != nil {
+		return config, fmt.Errorf("failed to set network config: %w", err)
+	}
+
+	return config, nil
+}
+
+// SetNetworkConfig finds the network CIDRs in the cluster if it is not set in the config
+// then sets them in the config
+func SetNetworkConfig(ctx context.Context, client client.Client, config Config) (Config, error) {
+	if config.NetworkConfig.ClusterNetworkCIDRBlock == "" {
+		clusterNetworkCIDRBlock, err := utils.GetClusterNetworkCIDRBlock(ctx, client)
+		if err != nil {
+			return config, fmt.Errorf("failed to get cluster network CIDR block: %w", err)
+		}
+		config.NetworkConfig.ClusterNetworkCIDRBlock = clusterNetworkCIDRBlock
+	}
+	if config.NetworkConfig.ServiceNetworkCIDRBlock == "" {
+		serviceNetworkCIDRBlock, err := utils.GetServiceNetworkCIDRBlock(ctx, client)
+		if err != nil {
+			return config, fmt.Errorf("failed to get service network CIDR block: %w", err)
+		}
+		config.NetworkConfig.ServiceNetworkCIDRBlock = serviceNetworkCIDRBlock
+	}
+	return config, nil
 }
